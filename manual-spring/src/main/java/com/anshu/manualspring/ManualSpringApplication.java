78d0ade1c5dc2db80401eafc51539c2541fcccf6
ohlyanAnshu <anshubha6@gmail.com>
2021-05-27 12:11:54 -0400

    Learning Spring Boot

---
+++ manual-spring/src/main/java/com/anshu/manualspring/ManualSpringApplication.java
@@ -0,0 +1,15 @@
+package com.anshu.manualspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualSpringApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ManualSpringApplication.class, args);
+
+	}
+
+}
